ad92aad2f31f339b6ceb4afa87f1eb8788f17248
ShapovalenkoD <ar4dima@gmail.com>
2017-08-21 08:49:27 +0300

    5kyu

---
+++ flatten().js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/flatten/train/javascript
+// My solution
+function flatten() {
+    let result = [];
+    for (var i = 0; i < arguments.length; i++) {
+        if (Array.isArray(arguments[i])) {
+            result = result.concat(flatten(...arguments[i]));
+        } else {
+            result.push(arguments[i])
+        };
+    } return result;
+};
+flatten(1, [2, 3], 4, 5, [6, [7]]);
+
+// Top solution
+function flatten(){
+    return [].slice.call(arguments).reduce(function(a,b){
+        return a.concat(Array.isArray(b) ? flatten.apply(null,b) : b);
+    },[]);
+}
