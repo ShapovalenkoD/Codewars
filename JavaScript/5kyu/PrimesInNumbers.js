023079d071a1a6119afadcb67f8145a3a71416c6
ShapovalenkoD <ar4dima@gmail.com>
2017-08-23 09:31:13 +0300

    Add

---
+++ JavaScript/5kyu/PrimesInNumbers.js
@@ -0,0 +1,28 @@
+// My solution
+function primeFactors(n){
+    let array = [];
+    let res = '';
+    for (let i = 2; i <= n; i++){
+        if (n % i == 0) {
+            n = n / i
+            array.push(i)
+            i = i - 1
+        }
+    }
+    for (let i = 0; i < array.length; i++) {
+        let count = 0;
+        for (let j = 0; j <= i; j++) {
+            if (array[i] == array[j]) {
+                count++
+            }
+        }
+        count != 1 ? res = res + "(" + array[i] + "**" + count +  ")" :
+                           res = res + "(" + array[i] + ")"
+    }
+    return res
+}
+
+// Top solution
+
+
+// Cleaver solution
