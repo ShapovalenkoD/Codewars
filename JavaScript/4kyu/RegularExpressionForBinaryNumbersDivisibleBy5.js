45e0327ff5c9b34ab11220e1830ba2bf0392392f
ShapovalenkoD <ar4dima@gmail.com>
2017-09-07 23:31:30 +0300

    Added files

---
+++ JavaScript/4kyu/RegularExpressionForBinaryNumbersDivisibleBy5.js
@@ -0,0 +1,5 @@
+// My solution
+const divisibleByFive = /^(1(10)*(0|11)(01*01|01*00(10)*(0|11))*1(0)*)+$/;
+
+// Top solution
+const divisibleByFive = /^(0|1(10)*(0|11)(01*01|01*00(10)*(0|11))*1)+$/; // partial solution
