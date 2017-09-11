0b40b03abe3bc46257f080e5e9da9c8c2d57ca47
ShapovalenkoD <ar4dima@gmail.com>
2017-09-11 20:56:10 +0300

    Added files

--- JavaScript/6kyu/.Template.js
+++ JavaScript/6kyu/.Template.js
@@ -1,50 +1,7 @@
 // My solution
-function greet(language) {
-    const country = {
-        english: 'Welcome',
-        czech: 'Vitejte',
-        danish: 'Velkomst',
-        dutch: 'Welkom',
-        estonian: 'Tere tulemast',
-        finnish: 'Tervetuloa',
-        flemish: 'Welgekomen',
-        french: 'Bienvenue',
-        german: 'Willkommen',
-        irish: 'Failte',
-        italian: 'Benvenuto',
-        latvian: 'Gaidits',
-        lithuanian: 'Laukiamas',
-        polish: 'Witamy',
-        spanish: 'Bienvenido',
-        swedish: 'Valkommen',
-        welsh: 'Croeso',
-        IP_ADDRESS_INVALID: 'Welcome',
-        IP_ADDRESS_NOT_FOUND: 'Welcome',
-        IP_ADDRESS_REQUIRED: 'Welcome'
-    }
-    return country[language]
-}
-// Top solution
-function greet(lang) {
-    return langs[lang]||langs['english'];
-}
 
-var langs = {
-    'english': 'Welcome',
-    'czech': 'Vitejte',
-    'danish': 'Velkomst',
-    'dutch': 'Welkom',
-    'estonian': 'Tere tulemast',
-    'finnish': 'Tervetuloa',
-    'flemish': 'Welgekomen',
-    'french': 'Bienvenue',
-    'german': 'Willkommen',
-    'irish': 'Failte',
-    'italian': 'Benvenuto',
-    'latvian': 'Gaidits',
-    'lithuanian': 'Laukiamas',
-    'polish': 'Witamy',
-    'spanish': 'Bienvenido',
-    'swedish': 'Valkommen',
-    'welsh': 'Croeso'
-};
+
+// Top solution
+
+
+// Cleaver solution
--- JavaScript/8kyu/Welcome!.js
+++ JavaScript/8kyu/Welcome!.js
@@ -1,7 +1,52 @@
 // My solution
+function greet(language) {
+    const country = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso',
+        IP_ADDRESS_INVALID: 'Welcome',
+        IP_ADDRESS_NOT_FOUND: 'Welcome',
+        IP_ADDRESS_REQUIRED: 'Welcome'
+    }
+    return country[language]
+}
 
 
 // Top solution
+function greet(lang) {
+    return langs[lang]||langs['english'];
+}
 
-
-// Cleaver solution
+var langs = {
+    'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'
+};
